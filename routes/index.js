6c44f427a71aa833428a718bfc638a70470bdd6a
Sanyam Agarwal <sanyam.agarwal94@gmail.com>
2021-04-16 15:11:07 +0530

    update contact us form. messages directly to email now

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+const nodemailer = require("nodemailer");
+var smtpTransport = require('nodemailer-smtp-transport');
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -8,7 +11,9 @@ var router = express.Router();
 
 
 router.get("/", (req, res) => {
-  res.render("home", {});
+  res.render("home", {
+    alert_msg: ""
+  });
 });
 
 
@@ -66,29 +71,45 @@ router.get('/testimonials', (req,res)=> {
 })
 
 
-// TODO: set up a mail forwarder here.
-router.post('/', (req,res)=> {
+router.post('/', (req, res) => {
 
-  // const MyData = {
+  const { firstname, lastname, email, message } = req.body;
 
-  //   name : req.body.contactus_name,
-  //   phone: req.body.contactus_phone,
-  //   message: req.body.contactus_yourmessage,
+  console.log(req.body);
 
-  // } 
+  var transporter = nodemailer.createTransport(smtpTransport({
+      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      auth: {
+          user: "7xopenchannel@gmail.com",
+          pass: "anyonecanhackthis"
+      }
+  }));
 
-  // const Data = contactusModel(MyData)
+  var mailOptions = {
+      from: email,
+      to: '7xstudy@gmail.com',
+      subject: 'mail from 7xstudy website contact form',
+      text: firstname + ' ' + lastname + ' says:\n' + message
+  };
 
-  // Data.save((err, docs)=> {
-  //   if(err){
-  //     console.log('ERROR ')
-  //   }
-  //   else{
-  //     console.log(docs)
-  //     res.redirect('/')
-  //   }
-  // })
+  transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+          console.log(error);
+          res.render('home', {
+              alert_msg: "Unable to send email"
+          })
+      } else {
+          console.log('Email sent: ' + info.response);
+          res.render('home', {
+              alert_msg: "Email Sent Sucessfully"
+          })
+      }
+  });
 
 })
+
+
 
 module.exports = router;
